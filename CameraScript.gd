0f73d8eb46ede022b9d97a5b26c7afed7e988ce8
ebi-chiri-oishi <ttt.hirohirohiro@gmail.com>
2026-05-30 22:56:34 +0900

    カメラ視点の調整とビルド設定の調整

--- CameraScript.gd
+++ CameraScript.gd
@@ -1,5 +1,5 @@
 extends Node
-@export	var saikoro : RigidBody3D
+@export	var SaikoroObject : RigidBody3D
 @export var camera : Camera3D
 func _physics_process(delta):
-	camera.position = saikoro.position + Vector3(0,5,10)
+	camera.position = SaikoroObject.position + Vector3(0,5,5)
