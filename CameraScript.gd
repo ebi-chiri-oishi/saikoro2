dbd4c9d7fe9afd8c2d30f152ce86b9d0242efe26
ebi-chiri-oishi <ttt.hirohirohiro@gmail.com>
2026-05-30 22:04:49 +0900

    Godot4.6へのアップグレードとノード名の変更

--- CameraScript.gd
+++ CameraScript.gd
@@ -1,6 +1,5 @@
 extends Node
+@export	var saikoro : RigidBody3D
+@export var camera : Camera3D
 func _physics_process(delta):
-	var saikoro = $SaikoroObject as RigidBody3D
-	var camera = $Camera3D as Camera3D
-	camera.position = saikoro.position + Vector3(0,5,5)
-	
+	camera.position = saikoro.position + Vector3(0,5,10)
